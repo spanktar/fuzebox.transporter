e3a8b101dfeab23a4932e0049ea0b10105446516
Spanky <spanky@kapanka.com>
2012-09-19 16:31:10 -0700

    Remove unnecessary dependencies

--- setup.py
+++ setup.py
@@ -33,6 +33,6 @@ setup(name='fuzebox.transporter',
       [z3c.autoinclude.plugin]
       target = plone
       """,
-      setup_requires=["PasteScript"],
-      paster_plugins=["ZopeSkel"],
+      #setup_requires=["PasteScript"],
+      #paster_plugins=["ZopeSkel"],
       )
