4fd615acbf69a13ba9cad227c562ed012ee496df
Spanky <spanky@kapanka.com>
2012-09-19 17:09:55 -0700

    Remove PDBs and NotFound error

--- fuzebox/transporter/adapters.py
+++ fuzebox/transporter/adapters.py
@@ -8,18 +8,13 @@ class TransporterTraverser(object):
     
     def __init__(self, context, request):
 
-        import pdb; pdb.set_trace()
-
         self.context = context
         self.objects = context['go']
         self.object_ids = self.objects.objectIds()
         self.request = request
     
     def publishTraverse(self, request, name):
-        
-        import pdb; pdb.set_trace()
-        
+                
         if name in self.object_ids:
             return self.objects.get(name).__of__(self.context)
-        else:
-            raise NotFound
+        
